90125acc7fbc75fbe6e098e775fe1161f22239b2
henkvanvoorst92 <henkvanvoorst92@gmail.com>
2024-05-29 11:05:56 -0700

    upd

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
         "torchaudio >= 2.1.0",
         "simpleitk",
         "scikit-learn>=1",
-        "scipy==1",
+        "scipy>=1.5",
         "pydicom",
         "seaborn==0.11",
         "pandas>=2",
